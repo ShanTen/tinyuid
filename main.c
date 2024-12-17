ef430a96cc45d3f9314e01a10332f085a71fe209
Shan10Code <jojogithub@gmail.com>
2024-12-17 14:39:05 +0530

    bug fix

--- main.c
+++ main.c
@@ -37,11 +37,15 @@ int main(int argc, char* argv[])
     int uidSize = 4;
     srand(time(NULL));
 
-    if ( strcmp(argv[1], "help") == 0 || argc > 2)
+    if(DBG) printf("Before strcmp() => argc is %d\n", argc);
+
+    if ( argc > 2 && strcmp(argv[1], "help") == 0)
     {
         printf("Usage: %s [length]\n", argv[0]);
         return -1;
     }
+
+    if(DBG) printf("Before atoi() => uid size is %d\n", uidSize);
 
     if (argc > 1)
         uidSize = atoi(argv[1]);
@@ -54,8 +58,10 @@ int main(int argc, char* argv[])
     if(res < 0){
         printf("Invalid TinyUID length.\n");
         printf("length should be between %d and %d characters\n", MIN_UID_SIZE, MAX_UID_SIZE);
+        printf("Usage: %s [length]\n", argv[0]);
         return -1;
     }
 
+    if(DBG) printf("length of uid: %lld\n", strlen(myUID.str));
     printf("%s\n", myUID.str);
 }
